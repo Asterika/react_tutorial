13c47001a2325d5dc21d1904b11ba24b599f1a0a
Krystal Williams <asterika@Krystals-MacBook-Pro.local>
2019-01-29 14:21:34 -0800

    Updates

---
+++ precache-manifest.ecb0d6c2d94de24e12d57d81d0e31fc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "edd63c9cd126e25acf84",
+    "url": "/static/js/main.edd63c9c.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "edd63c9cd126e25acf84",
+    "url": "/static/css/main.253acbde.chunk.css"
+  },
+  {
+    "revision": "247c24fc9999891460434d6fd721ad45",
+    "url": "/index.html"
+  }
+];
